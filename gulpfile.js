ffb1f42f63d6bcb2c6dace84b6062856a467b7b3
FeanorD <meleshkodim2000@gmail.com>
2020-06-17 12:42:48 +0300

    modify gulpfile and end markup for desktop

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,23 @@ const plumber = require('gulp-plumber');
 const notify = require('gulp-notify');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-// const watch = require('gulp-watch');
+const fileinclude = require('gulp-file-include');
 
 function server() {
-	
 	browserSync.init({
 		server: { baseDir: './app/'}
 	});
 
-	// watch('./app/**/*.html', browserSync.stream());
-	// watch('./app/**/*.js', browserSync.reload());
-	// watch('./app/img/*.*', browserSync.reload());
+	watch([
+		'./app/**/*.html',
+		'./app/**/*.js',
+		'./app/img/*.*',
+		'./app/less/**/*.less'
+	], series(styles))
+		.on('change', browserSync.reload);
 
-
-	watch(['./app/**/*.html', './app/**/*.js', './app/img/*.*', './app/less/**/*.less'], series(styles)).on('change', browserSync.reload);
-
-
-	// watch('./app/less/**/*.less', function(){
-	// 	start('styles');
-	// });
-
+	watch('./app/less/**/*.less', series(styles))
+	watch('./app/html/**/*.html', series(html))
 }
 
 function styles() {
@@ -49,7 +46,22 @@ function styles() {
 	.pipe(browserSync.stream());
 }
 
-// gulp.task('default', ['server']);
+function html() {
+	return src('./app/html/*.html')
+		.pipe(plumber({
+			errorHandler: notify.onError(function(err){
+				return {
+					title: 'HTML include',
+					sound: false,
+					message: err.message
+				}
+			})
+		}))
+		.pipe(fileinclude({
+			prefix: '@@'
+		}))
+		.pipe(dest('./app/'))
+}
 
 exports.styles = styles
 exports.default = series(styles, server)
