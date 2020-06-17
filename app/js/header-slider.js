4b6392818541cc094660b052834fc41b929f5846
FeanorD <meleshkodim2000@gmail.com>
2020-06-17 23:10:06 +0300

    add OwlCarousel and fix bg-lines

---
+++ app/js/header-slider.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  const headerSlider = $("#headerSlider");
+
+  headerSlider.on('initialized.owl.carousel', event => {
+    $('.slide-controls-number__active').text(event.item.index + 1);
+    $('.slide-controls-number__total').text(event.item.count);
+  })
+
+  headerSlider.owlCarousel({
+    items: 1,
+    // loop: true,
+    dots: false,
+    smartSpeed: 1200,
+  });
+
+  $('#headerSliderRight').click(function() {
+    headerSlider.trigger('next.owl.carousel');
+  })
+
+  $('#headerSliderLeft').click(function() {
+    headerSlider.trigger('prev.owl.carousel');
+  })
+
+  headerSlider.on('changed.owl.carousel', event => {
+    $('.slide-controls-number__active').text(event.item.index + 1);
+    $('.slide-controls-number__total').text(event.item.count);
+  })
+
+});
---
+++ app/js/shop-slider.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  const shopSlider = $("#shopSlider");
+
+  shopSlider.owlCarousel({
+    items: 3,
+    loop: true,
+    dots: false,
+    smartSpeed: 500,
+    margin: 2,
+  });
+
+  $('#shopSliderRight').click(function() {
+    shopSlider.trigger('next.owl.carousel');
+  })
+
+  $('#headerSliderLeft').click(function() {
+    shopSlider.trigger('prev.owl.carousel');
+  })
+
+});
